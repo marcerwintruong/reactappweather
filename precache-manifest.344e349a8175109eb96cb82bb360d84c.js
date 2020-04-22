116ce60c25a694d2e30c32df551241c597ddeb8c
Jakob <greaterjakob@gmail.com>
2020-04-22 12:21:54 +0800

    Updates

---
+++ precache-manifest.344e349a8175109eb96cb82bb360d84c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330ccbdba789bdca5f5055acf6a105f5",
+    "url": "/reactappweather/index.html"
+  },
+  {
+    "revision": "97117dcd025953d1e498",
+    "url": "/reactappweather/static/css/main.7bdb5d2e.chunk.css"
+  },
+  {
+    "revision": "574f5e58a80dbee3a3e0",
+    "url": "/reactappweather/static/js/2.d74e0e5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactappweather/static/js/2.d74e0e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97117dcd025953d1e498",
+    "url": "/reactappweather/static/js/main.1c953a6e.chunk.js"
+  },
+  {
+    "revision": "bd0ee4e844385de730a7",
+    "url": "/reactappweather/static/js/runtime-main.8ad60b53.js"
+  }
+]);
