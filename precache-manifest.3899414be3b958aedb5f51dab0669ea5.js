2ad2189cb9557cd4ff406fff99f7fc13c9cbf2d0
Jakob <greaterjakob@gmail.com>
2020-04-27 13:50:43 +0800

    Updates

---
+++ precache-manifest.3899414be3b958aedb5f51dab0669ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67b5c3dae307a49fc2be1026b1a12de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f2b301c4be0e01e992",
+    "url": "/static/css/main.7ab5ed01.chunk.css"
+  },
+  {
+    "revision": "bd2ae40a4f51035e2ad7",
+    "url": "/static/js/2.ca16e0cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca16e0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f2b301c4be0e01e992",
+    "url": "/static/js/main.cd8482a6.chunk.js"
+  },
+  {
+    "revision": "68bdce13f224ea00ec8d",
+    "url": "/static/js/runtime-main.b2c099ef.js"
+  }
+]);
